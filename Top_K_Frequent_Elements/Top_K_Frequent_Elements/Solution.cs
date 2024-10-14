3e75afc0ea732d723441fc5219db99a77a1ac6ed
soc day <socpro13579@gmail.com>
2024-10-14 16:48:05 +0700

    Add Top_K_Frequent comment after test a lot

--- Top_K_Frequent_Elements/Top_K_Frequent_Elements/Solution.cs
+++ Top_K_Frequent_Elements/Top_K_Frequent_Elements/Solution.cs
@@ -8,22 +8,18 @@ public class Solution {
 
     public int[] Solution1(int[] nums, int k)
     {
-        //Mot mang chua so va so luong count
-        // So nao nhieu hon thi len dau
-        // Tra lai mang theo so luong tu dau den k
 
+        //Beat 35%, 136 ms, memory 50mb
+        //Test case is random and ms and memory depent a lot on test case..
+        if (nums.Length == k) return nums;
         Dictionary<int, int> d = new Dictionary<int, int>();
         for (int i = 0; i < nums.Length; i++)
-        {
+        {   
+            // Maybe time consume here.
             if (!d.ContainsKey(nums[i]))
-            {
-                d.Add(nums[i], 1);
-            }
+                d[nums[i]] = 1;
             else
-            {
                 d[nums[i]]++;
-            }
-
         }
         return d.OrderByDescending(x => x.Value)
             .Take(k)
@@ -37,24 +33,54 @@ public class Solution {
         // So nao nhieu hon thi len dau
         // Tra lai mang theo so luong tu dau den k
 
-        //Tao mang theo so luong cua K
+        // Tao mang theo so luong cua K
         // Sort 
-        Dictionary<int, int> d = new Dictionary<int, int>();
-        for (int i = 0; i < nums.Length; i++)
+        // Lay so dau tien cho no la lon nhat
+        // Neu so tiep theo lon hon cho qua thu hai, neu no ra khoi mang cut
+        // Error and feel ineffective.
+        List<Dictionary<int, int>> list = [];
+        Array.Sort (nums);
+        int count = 0, temp =0;
+
+        if (nums.Length == k) return nums;
+
+        // Khi mang chua full add vo boi vi chac chan k phai full
+
+        while (temp != nums.Length - 1)
         {
-            if (!d.ContainsKey(nums[i]))
+            while (list.Count < k)
             {
-                d.Add(nums[i], 1);
-            }
-            else
-            {
-                d[nums[i]]++;
+                for (int i = temp; i < nums.Length; i++)
+                {
+                    if (nums[i] != nums[i] + 1)
+                    {
+                        list.Add(new Dictionary<int, int> { { nums[i], count } });
+                        temp = i;
+                        count = 0;
+                        break;
+                    }
+                    count++;
+                }
             }
 
+            while (list.Count == k)
+            {
+                list = list.OrderByDescending(dict => dict.Values.First()).ToList();
+                for (int i = temp; i < nums.Length; i++)
+                {
+                    if (nums[i] != nums[i] + 1)
+                    {
+                        list[^1] = (new Dictionary<int, int> { { nums[i], count } });
+                        temp = i;
+                        count = 0;
+                        break;
+                    }
+                    count++;
+                }
+            }
+            
         }
-        return d.OrderByDescending(x => x.Value)
-            .Take(k)
-            .Select(x => x.Key)
-            .ToArray();
+        int[] keys = list.Select(dict => dict.Keys.First()).ToArray();
+        return keys;
     }
 }
